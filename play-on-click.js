65f0817eea3bbaadfb525be26405e8c844ad60a9
daniel cheang <daniel@cpttm.org.mo>
2023-09-15 18:30:24 +0800

    try to use entry instead of video

--- play-on-click.js
+++ play-on-click.js
@@ -10,11 +10,9 @@ AFRAME.registerComponent('play-on-click', {
     window.removeEventListener('click', this.onClick);
   },
   onClick: function (evt) {
-    // var videoEl = this.el.getAttribute('material').src;
-    // if (!videoEl) { return; }
-    // this.el.object3D.visible = true;
-    // videoEl.play();
-		const el = document.querySelector("#videoFireworks")
-		if (el) el.play()
+    var videoEl = this.el.getAttribute('material').src;
+    if (!videoEl) { return; }
+    this.el.object3D.visible = true;
+    videoEl.play();
   }
 });
